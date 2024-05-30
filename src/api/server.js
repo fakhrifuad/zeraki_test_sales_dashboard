0b304292b10e0e5b58b74d7a6bed0844327f09c7
fakhrifuad <fakhrialamry@gmail.com>
2024-05-30 15:27:40 +0300

    refactor: try to fix deployment with database

--- src/api/server.js
+++ src/api/server.js
@@ -1,9 +1,13 @@
+// JSON Server module
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('src/data/db.json');
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use('/api', router);
 
+// Export the Server API
 module.exports = server;
